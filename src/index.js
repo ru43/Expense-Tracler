a5f3a1bf3e43ba78b06fe8595223d034ffce8472
Rupesh Kumar <55132245+ru43@users.noreply.github.com>
2022-04-05 16:00:46 +0530

    uploading project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+// First file that will execute at first
+
+
+// The below code rendeers app instead of the root element find in the indexedDB.html file in [ublic]
+ReactDOM.render(<App />, document.getElementById('root'));
+
